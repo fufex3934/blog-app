5bce3f8b97ff99e6becd4cc3c25e84a51f73b4c7
fufex3934 <fufawakgari174@gmail.com>
2024-12-01 17:33:42 +0300

    ff

--- src/database/index.jsx
+++ src/database/index.jsx
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
-import {dotenv} from "dotenv";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const connectToDB = async () => {
-    dotenv.config();
   const connectionUrl = process.env.MONGODB_URI || "mongodb+srv://fufawakgari174:1234@cluster0.b7iu3.mongodb.net/";
-    
-  
-  mongoose
-    .connect(connectionUrl)
-    .then(() => console.log("blog database connection is success"))
-    .catch((error) => console.log(error.message));
+
+  try {
+    await mongoose.connect(connectionUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Blog database connection is successful");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }
 };
 
 export default connectToDB;
