ccb008ed8afd83ceced2295c582631a468b0da26
fufex3934 <fufawakgari174@gmail.com>
2024-12-01 13:56:36 +0300

    post api routes

--- src/app/components/add-new-blog/index.jsx
+++ src/app/components/add-new-blog/index.jsx
@@ -1,22 +1,37 @@
-'use client';
+"use client";
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-  } from "@/components/ui/dialog";
-  import { Input } from "@/components/ui/input";
-  import { Label } from "@/components/ui/label";
-const AddNewBlog = ({openBlogDialog,setOpenBlogDialog}) => {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const AddNewBlog = ({
+  openBlogDialog,
+  setOpenBlogDialog,
+  loading,
+
+  setBlogFormData,
+  blogFormData,
+  handleSaveBlogData,
+}) => {
   return (
     <>
-     <div>
+      <div>
         <Button onClick={() => setOpenBlogDialog(true)}>Add New Blog</Button>
       </div>
-      
-      <Dialog open={openBlogDialog} onOpenChange={setOpenBlogDialog}>
+
+      <Dialog
+        open={openBlogDialog}
+        onOpenChange={() => {
+          setOpenBlogDialog(false);
+          setBlogFormData({ title: "", description: "" });
+        }}
+      >
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Add New Blog</DialogTitle>
@@ -26,22 +41,48 @@ const AddNewBlog = ({openBlogDialog,setOpenBlogDialog}) => {
               <Label htmlFor="title" className="text-right">
                 Title
               </Label>
-              <Input id="title" className="col-span-3" />
+              <Input
+                name="title"
+                onChange={(e) =>
+                  setBlogFormData({
+                    ...blogFormData,
+                    title: e.target.value,
+                  })
+                }
+                value={blogFormData.title}
+                placeholder="Enter Blog Title"
+                id="title"
+                className="col-span-3"
+              />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="description" className="text-right">
                 Description
               </Label>
-              <Input id="description" className="col-span-3" />
+              <Input
+                name="description"
+                onChange={(e) =>
+                  setBlogFormData({
+                    ...blogFormData,
+                    description: e.target.value,
+                  })
+                }
+                value={blogFormData.description}
+                placeholder="Enter Blog Description"
+                id="description"
+                className="col-span-3"
+              />
             </div>
           </div>
           <DialogFooter>
-            <Button type="button">Save changes</Button>
+            <Button type="button" onClick={handleSaveBlogData}>
+              {loading ? "Saving changes" : "Save changes"}
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
-  )
-}
+  );
+};
 
-export default AddNewBlog
+export default AddNewBlog;
--- src/app/components/blog-overview/index.jsx
+++ src/app/components/blog-overview/index.jsx
@@ -2,14 +2,52 @@
 
 import React, { useState } from "react";
 
-
 import AddNewBlog from "../add-new-blog";
+const initialBlogFormData = {
+  title: "",
+  description: "",
+};
 const BlogOverview = () => {
   const [openBlogDialog, setOpenBlogDialog] = useState(false);
+  const [blogFormData, setBlogFormData] = useState(initialBlogFormData);
+  const [loading, setLoading] = useState(false);
+
+  async function handleSaveBlogData() {
+    try {
+      setLoading(true);
+      const apiResponse = await fetch("/api/add-blog", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(blogFormData),
+      });
+      const result = await apiResponse.json();
+      
+      if (result.success) {
+        setBlogFormData(initialBlogFormData);
+        setOpenBlogDialog(false);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setBlogFormData(initialBlogFormData);
+    }
+  }
+
   return (
     <div className="min-h-screen flex flex-col gap-10  p-6">
-     <AddNewBlog openBlogDialog={openBlogDialog} setOpenBlogDialog={setOpenBlogDialog}/>
-     <div>Blog List Section</div>
+      <AddNewBlog
+        openBlogDialog={openBlogDialog}
+        setOpenBlogDialog={setOpenBlogDialog}
+        loading={loading}
+        setLoading={setLoading}
+        blogFormData={blogFormData}
+        setBlogFormData={setBlogFormData}
+        handleSaveBlogData={handleSaveBlogData}
+      />
+      <div>Blog List Section</div>
     </div>
   );
 };
